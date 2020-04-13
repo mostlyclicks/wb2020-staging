adfca24e8f86e77bb684e937958c5825d9573918
mostlyclicks <carlos@mostlyclicks.com>
2020-04-13 06:54:05 -0600

    construction management

---
+++ src/pages/services/construction-management.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/layout'
+
+const ConstructionManagement = () => {
+  return (
+    <Layout>
+      <h1>Construction Management</h1>
+      <p>Construction Management copy</p>
+    </Layout>
+  )
+}
+
+export default ConstructionManagement
