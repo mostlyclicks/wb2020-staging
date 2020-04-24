0c37ec3e48ff83cd9bf8a27fe333fb32b0f9ccfe
mostlyclicks <carlos@mostlyclicks.com>
2020-04-24 07:35:58 -0600

    add contact form and thankyou page

---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const ThankYou = () => (
+  <Layout>
+    <div>
+      <h1>Thank you.</h1>
+    </div>
+  </Layout>
+)
+
+export default ThankYou
